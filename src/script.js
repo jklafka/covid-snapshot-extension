e0253a539b72735c15971c6e6e7fd8da4028d840
Josef Klafka <jlklafka@gmail.com>
2022-11-02 16:15:43 -0700

    start transition to us data

--- src/script.js
+++ src/script.js
@@ -1,10 +1,19 @@
+/**
+* Fetch COVID data from the US API endpoint
+*/
 async function fetchData() {
-  const sourceUrl = "https://api.coronavirus.data.gov.uk/v1/data";
-  const res = await fetch(sourceUrl);
-  const record = await res.json();
-  document.getElementById("date").innerHTML=record.data[0].date;
-  document.getElementById("areaName").innerHTML=record.data[0].areaName;
-  document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-  document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+  const sourceUrl = "https://github.com/nytimes/covid-19-data/blob/master/live/us.csv";
+  const res = await fetch(sourceUrl, {
+      method: 'get',
+      headers: {
+        'content-type': 'text/csv;charset=UTF-8'
+      }
+  });
+  const record = await res.text();
+
+  document.getElementById("date").innerHTML = record.data[0].date;
+  document.getElementById("areaName").innerHTML = record.data[0].areaName;
+  document.getElementById("latestBy").innerHTML = record.data[0].latestBy;
+  document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
 }
 fetchData();
